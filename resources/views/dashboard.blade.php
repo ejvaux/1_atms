c8b48ae509f3b3e76fc732a591f9e6d0985e05db
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-07-18 12:00:19 +0800

    creating forms

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
+@section('pageTitle','Dashboard | TMS - Primatech')
+
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -18,6 +20,23 @@
                 </div>
             </div>
         </div>
+    </div> --}}
+
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#create" role="tab" >Create Ticket</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#view" role="tab" >View Ticket</a>
+        </li>
+        {{-- <li class="nav-item">
+            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" >Contact</a>
+        </li> --}}
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="create" role="tabpanel">@include('inc.ct')</div>
+        <div class="tab-pane fade" id="view" role="tabpanel">@include('inc.vt')</div>
+        {{-- <div class="tab-pane fade" id="contact" role="tabpanel">tab 3</div> --}}
     </div>
 </div>
 @endsection
