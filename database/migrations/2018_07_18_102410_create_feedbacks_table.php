fb30f3dec664a5c60a035d6eb9cee32994e76c08
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-07-25 08:02:16 +0800

    Modified migration files

--- database/migrations/2018_07_18_102410_create_feedbacks_table.php
+++ database/migrations/2018_07_18_102410_create_feedbacks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFeedbackTable extends Migration
+class CreateFeedbacksTable extends Migration
 {
     /**
      * Run the migrations.
