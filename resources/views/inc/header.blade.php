3fa57847f9a438db9fc72568a9d40e9a921e5b6b
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-07-25 09:15:45 +0800

    modified header

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -7,26 +7,26 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 <!-- Izitoast -->
-<link rel="stylesheet" href="/1_tms/node_modules/izitoast/dist/css/iziToast.min.css">
+<link rel="stylesheet" href="../../node_modules/izitoast/dist/css/iziToast.min.css">
 
 <!-- NProgress -->
-<link rel='stylesheet' href='/1_tms/node_modules/nprogress/nprogress.css'/>
+<link rel='stylesheet' href='../../node_modules/nprogress/nprogress.css'/>
 
 
 <!-- ______________________ Scripts ______________________ -->
 <script src="{{ asset('js/app.js') }}" defer></script>
 
 <!-- Jquery -->
-<script src="/1_tms/node_modules/jquery/dist/jquery.min.js" type="text/javascript"></script>
+<script src="../../node_modules/jquery/dist/jquery.min.js" type="text/javascript"></script>
 
 <!-- Izitoast -->
-<script src="/1_tms/node_modules/izitoast/dist/js/iziToast.min.js" type="text/javascript"></script>
+<script src="../../node_modules/izitoast/dist/js/iziToast.min.js" type="text/javascript"></script>
 
 <!-- NProgress -->
-<script src='/1_tms/node_modules/nprogress/nprogress.js'></script>
+<script src='../../node_modules/nprogress/nprogress.js'></script>
 
 <!-- Pjax -->
-<script src="/1_tms/node_modules/pjax/pjax.min.js" defer></script>
+<script src="../../node_modules/pjax/pjax.min.js" defer></script>
 
 <!-- Custom -->
 <script src="{{ asset('js/dashboard.js') }}" defer></script>
