1c9b8594d256411cece4d46290c7653df9b3c9f4
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-07-25 08:27:19 +0800

    Added custom 503 page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,7 @@
+@extends('errors::layout')
+
+@section('title', 'Service Unavailable')
+
+{{-- @section('message', 'Be right back.') --}}
+
+@section('message', 'Setting Up and Building Database. Come Back Later. :)')
