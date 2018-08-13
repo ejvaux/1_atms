91033797435637baf8234ec6cf547d2c45c32918
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-08-13 08:28:53 +0800

    Modified class - migration

--- database/migrations/2018_08_10_114229_edit_columns_closed_ticket_table2.php
+++ database/migrations/2018_08_10_114229_edit_columns_closed_ticket_table2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EditColumnsClosedTicketTable extends Migration
+class EditColumnsClosedTicketTable2 extends Migration
 {
     /**
      * Run the migrations.
