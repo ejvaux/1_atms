b245bf9fbdb6161859ff01ed8852d8b7be3d873d
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-09-26 08:20:10 +0800

    fixed bug on sorting

--- app/ClosedTicket.php
+++ app/ClosedTicket.php
@@ -7,6 +7,8 @@ use Kyslik\ColumnSortable\Sortable;
 
 class ClosedTicket extends Model
 {
+    use Sortable;
+
     public $sortable = ['id', 'user_id', 'department_id','category_id','priority_id','status_id','subject','assigned_to','start_at','finish_at', 'created_at', 'updated_at'];
 
     public function user()
