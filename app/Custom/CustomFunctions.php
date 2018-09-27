53d8d6397495293f50d51289d0c21ad88fbda933
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-09-27 11:57:27 +0800

    clean

--- app/Custom/CustomFunctions.php
+++ app/Custom/CustomFunctions.php
@@ -18,32 +18,25 @@ class CustomFunctions
         switch($stat){
             case 1:
                 $form = "<span class='badge badge-danger'>" . $html->name . "</span>";
-                break;
-        
+                break;        
             case 2:
                 $form = "<span class='badge badge-info'>" . $html->name . "</span>";
                 break;
-
             case 3:
                 $form = "<span class='badge badge-primary'>" . $html->name . "</span>";
                 break;
-
             case 4:
                 $form = "<span class='badge badge-warning'>" . $html->name . "</span>";
                 break;
-
             case 5:
                 $form = "<span class='badge badge-success'>" . $html->name . "</span>";
                 break;
-
             case 6:
                 $form = "<span class='badge badge-secondary'>" . $html->name . "</span>";
-                break;
-            
+                break;            
             case 7:
                 $form = "<span class='badge badge-secondary'>" . $html->name . "</span>";
-                break;
-        
+                break;        
             default:
                 $form = "<span>Something went wrong, please try again</span>";
         }
@@ -55,24 +48,19 @@ class CustomFunctions
         switch($prio){
             case 1:
                 $form = "<span class='badge badge-success'>" . $html->name . "</span>";
-                break;
-        
+                break;        
             case 2:
                 $form = "<span class='badge badge-info'>" . $html->name . "</span>";
                 break;
-
             case 3:
                 $form = "<span class='badge badge-primary'>" . $html->name . "</span>";
                 break;
-
             case 4:
                 $form = "<span class='badge badge-warning'>" . $html->name . "</span>";
                 break;
-
             case 5:
                 $form = "<span class='badge badge-danger'>" . $html->name . "</span>";
-                break;
-        
+                break;        
             default:
                 $form = "<span>Something went wrong, please try again</span>";
         }
@@ -85,32 +73,25 @@ class CustomFunctions
         switch($stat){
             case 1:
                 $form = "<span class='text-danger'>" . $html->name . "</span>";
-                break;
-        
+                break;        
             case 2:
                 $form = "<span class='text-info'>" . $html->name . "</span>";
                 break;
-
             case 3:
                 $form = "<span class='text-primary'>" . $html->name . "</span>";
                 break;
-
             case 4:
                 $form = "<span class='text-warning'>" . $html->name . "</span>";
                 break;
-
             case 5:
                 $form = "<span class='text-success'>" . $html->name . "</span>";
                 break;
-
             case 6:
                 $form = "<span class='text-secondary'>" . $html->name . "</span>";
                 break;
-
             case 7:
                 $form = "<span class='text-secondary'>" . $html->name . "</span>";
-                break;
-        
+                break;        
             default:
                 $form = "<span>Something went wrong, please try again</span>";
         }
@@ -257,24 +238,19 @@ class CustomFunctions
         switch($stat){
             case 1:
                 $form = "<span class='text-danger'>" . $html->name . "</span>";
-                break;
-        
+                break;        
             case 2:
                 $form = "<span class='text-info'>" . $html->name . "</span>";
                 break;
-
             case 3:
                 $form = "<span class='text-primary'>" . $html->name . "</span>";
                 break;
-
             case 4:
                 $form = "<span class='text-warning'>" . $html->name . "</span>";
                 break;
-
             case 5:
                 $form = "<span class='text-success'>" . $html->name . "</span>";
-                break;            
-        
+                break; 
             default:
                 $form = "<span>Something went wrong, please try again</span>";
         }
@@ -286,24 +262,19 @@ class CustomFunctions
         switch($stat){
             case 1:
                 $form = "<span class='badge badge-danger'>" . $html->name . "</span>";
-                break;
-        
+                break;        
             case 2:
                 $form = "<span class='badge badge-info'>" . $html->name . "</span>";
                 break;
-
             case 3:
                 $form = "<span class='badge badge-primary'>" . $html->name . "</span>";
                 break;
-
             case 4:
                 $form = "<span class='badge badge-warning'>" . $html->name . "</span>";
                 break;
-
             case 5:
                 $form = "<span class='badge badge-success'>" . $html->name . "</span>";
                 break;
-
             default:
                 $form = "<span>Something went wrong, please try again</span>";
         }
